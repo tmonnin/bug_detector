c695059962b93423f1b447245b056d4d3ddb8ee7
Thomas Monninger <t.monninger@t-online.de>
2020-07-17 10:01:29 +0200

    Outsource other strategies, clear code, improve saving

--- starter_kit/model_lstm.py
+++ starter_kit/model_lstm.py
@@ -6,41 +6,17 @@ from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_se
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
 
+import utils
+
 class LSTMNet(nn.Module):
 
     def __init__(self, num_features=13, num_classes=1):
         super(LSTMNet, self).__init__()
-        # TODO check if necessary
-        # 4 special characters: < pad >, EOS, < unk >, N
-        # https://suzyahyah.github.io/pytorch/2019/07/01/DataLoader-Pad-Pack-Sequence.html
+
         self.embedding = nn.Embedding(num_features, 50)
-        strategy = "lstm"
-        if strategy == "graph_conv":
-            self.conv1 = GCNConv(num_features, 128, cached=True, normalize=False)
-            self.conv2 = GCNConv(128, 9, cached=True, normalize=False)
-            self.conv3 = GCNConv(9, 5, cached=True, normalize=False)
-            self.conv4 = GCNConv(5, num_classes, cached=True, normalize=False)
 
-            self.reg_params = self.conv1.parameters()
-            self.non_reg_params = self.conv2.parameters()
-
-        elif strategy == "conv":
-            # TODO 1D conv
-            self.conv1 = nn.Conv2d(150, 32, (1, 5), stride=(1,1))#, padding=(0,2))
-            self.conv2 = nn.Conv2d(32, 64, (1, 3), stride=(1,1))
-            self.conv3 = nn.Conv2d(64, 64, (1, 3), stride=(1,1))
-            self.conv4 = nn.Conv2d(64, 64, (1, 3), stride=(1,1))
-
-        elif strategy == "lstm":
-            self.lstm_condition = nn.LSTM(150, 256, num_layers=1, bidirectional=False, batch_first=True) # TODO potentially increase e.g. 512
-            self.lstm_context = nn.LSTM(100, 128, num_layers=1, bidirectional=False, batch_first=True) # TODO potentially increase e.g. 512
-            # TODO padding
-            # 5 prev and post lines
-            # Fancy: attention
-            # Type context: Vergrößern oder eigenes Embedding
-            # BUT TYPE NOT RELEVANT for wrong operator task
-            # Output: 10x lstm_context, 1x lstm_condition
-            # Concat, linear, auf 128
+        self.lstm_condition = nn.LSTM(150, 256, num_layers=1, bidirectional=False, batch_first=True) # TODO potentially increase e.g. 512
+        self.lstm_context = nn.LSTM(100, 128, num_layers=1, bidirectional=False, batch_first=True) # TODO potentially increase e.g. 512
 
         self.lin_condition = nn.Linear(256, 128)
         self.lin_context = nn.Linear(128, 64)
@@ -54,63 +30,32 @@ class LSTMNet(nn.Module):
 
     def forward(self, type_batch_pad, property_batch_pad, token_batch_pad, pad_lens, pad_token_lens):
 
-        strategy = "lstm"
-        #for type_len, type_pad in zip(type_lens, type_batch_pad):
         type_embedded = self.embedding(torch.tensor(type_batch_pad).to(torch.int64))
         x_condition = torch.cat([type_embedded, property_batch_pad], dim=2) # B,S,C
 
-        if strategy == "conv":
-            type_embedded = torch.transpose(type_embedded, 1, 2)
-            type_embedded = type_embedded.unsqueeze(2)
+        packed_input = pack_padded_sequence(x_condition, pad_lens, enforce_sorted=False, batch_first=True)
+        packed_outputs, _ = self.lstm_condition(packed_input)#, input_memory)
+        x_condition, outputs_len = pad_packed_sequence(packed_outputs, batch_first=True)
+        # (batch_size) -> (batch_size, 1, 1)
+        lengths = pad_lens.unsqueeze(1).unsqueeze(2)
+        # (batch_size, 1, 1) -> (batch_size, 1, hidden_size)
+        lengths = lengths.expand((-1, 1, x_condition.size(2)))
+        # (batch_size, seq, hidden_size) -> (batch_size, 1, hidden_size)
+        x_condition = torch.gather(x_condition, 1, lengths - 1)
+        # (batch_size, 1, hidden_size) -> (batch_size, hidden_size)
+        x_condition = x_condition.squeeze(1)
 
-            x_condition = torch.cat((type_embedded, property_embedded), dim=1)
-
-            x_condition = F.relu(self.conv1(x_condition))
-            #x = F.dropout(x, training=self.training)
-            x_condition = F.relu(self.conv2(x_condition))
-            x_condition = F.relu(self.conv3(x_condition))
-            x_condition = F.relu(self.conv4(x_condition))
-            #x = x[0] # select weights from root node
-            x_condition = x_condition.view(-1, 64*1*5)
-            x_condition = F.relu(self.lin1(x_condition))
-
-        if strategy == "graph_conv":
-            [type_int, property] = data  # , edge_index, edge_weight = data.x, data.edge_index, data.edge_attr
-            x_condition = F.relu(self.conv1(x_condition), edge_index, edge_weight)
-            # x = F.dropout(x, training=self.training)
-            x_condition = F.relu(self.conv2(x_condition), edge_index, edge_weight)
-            x_condition = F.relu(self.conv3(x_condition), edge_index, edge_weight)
-            x_condition = F.relu(self.conv4(x_condition), edge_index, edge_weight)
-            # x = x[0] # select weights from root node
-            x_condition = x_condition.view(-1, 64 * 1 * 5)
-            x_condition = F.relu(self.lin1(x_condition))
-
-        if strategy == "lstm":
-            packed_input = pack_padded_sequence(x_condition, pad_lens, enforce_sorted=False, batch_first=True)
-            packed_outputs, _ = self.lstm_condition(packed_input)#, input_memory)
-            x_condition, outputs_len = pad_packed_sequence(packed_outputs, batch_first=True)
-            # (batch_size) -> (batch_size, 1, 1)
-            lengths = pad_lens.unsqueeze(1).unsqueeze(2)
-            # (batch_size, 1, 1) -> (batch_size, 1, hidden_size)
-            lengths = lengths.expand((-1, 1, x_condition.size(2)))
-            # (batch_size, seq, hidden_size) -> (batch_size, 1, hidden_size)
-            x_condition = torch.gather(x_condition, 1, lengths - 1)
-            # (batch_size, 1, hidden_size) -> (batch_size, hidden_size)
-            x_condition = x_condition.squeeze(1)
-
-
-            packed_context_in = torch.nn.utils.rnn.pack_padded_sequence(token_batch_pad, pad_token_lens, enforce_sorted=False, batch_first=True)
-            packed_context_out, _ = self.lstm_context(packed_context_in)
-            x_context, outputs_len = torch.nn.utils.rnn.pad_packed_sequence(packed_context_out, batch_first=True)
-            # (batch_size) -> (batch_size, 1, 1)
-            lengths = pad_token_lens.unsqueeze(1).unsqueeze(2)
-            # (batch_size, 1, 1) -> (batch_size, 1, hidden_size)
-            lengths = lengths.expand((-1, 1, x_context.size(2)))
-            # (batch_size, seq, hidden_size) -> (batch_size, 1, hidden_size)
-            x_context = torch.gather(x_context, 1, lengths - 1)
-            # (batch_size, 1, hidden_size) -> (batch_size, hidden_size)
-            x_context = x_context.squeeze(1)
-
+        packed_context_in = torch.nn.utils.rnn.pack_padded_sequence(token_batch_pad, pad_token_lens, enforce_sorted=False, batch_first=True)
+        packed_context_out, _ = self.lstm_context(packed_context_in)
+        x_context, outputs_len = torch.nn.utils.rnn.pad_packed_sequence(packed_context_out, batch_first=True)
+        # (batch_size) -> (batch_size, 1, 1)
+        lengths = pad_token_lens.unsqueeze(1).unsqueeze(2)
+        # (batch_size, 1, 1) -> (batch_size, 1, hidden_size)
+        lengths = lengths.expand((-1, 1, x_context.size(2)))
+        # (batch_size, seq, hidden_size) -> (batch_size, 1, hidden_size)
+        x_context = torch.gather(x_context, 1, lengths - 1)
+        # (batch_size, 1, hidden_size) -> (batch_size, hidden_size)
+        x_context = x_context.squeeze(1)
 
         x_condition = F.relu(self.lin_condition(x_condition))
         x_context = F.relu(self.lin_context(x_context))
@@ -119,8 +64,11 @@ class LSTMNet(nn.Module):
         x = torch.sigmoid(x)
         return x
 
-    def train(self, train_set, learning_rate, epochs, weights):
+    def train(self, train_set, learning_rate, epochs, distribution):
         training_set = TrainLoader(train_set)
+        labels = []
+        [labels.append(data["label"]) for data in train_set]
+        weights = utils.weighted_distribution(labels, distribution)
         assert len(training_set) == len(weights)
         weighted_sampler = torch.utils.data.WeightedRandomSampler(weights, len(weights), replacement=True)
         training_generator = torch.utils.data.DataLoader(training_set, **self.params, collate_fn=training_set.pad_collate, sampler=weighted_sampler)
@@ -130,7 +78,7 @@ class LSTMNet(nn.Module):
         # create a loss function
 
         # run the main training loop
-        for epoch in range(epochs):
+        for epoch in range(1, epochs+1):
             loss_avg = 0
             count_correct = 0.0
             count_wrong = 0.0
@@ -158,7 +106,7 @@ class LSTMNet(nn.Module):
             loss_avg = loss_avg/i
             print('Train Epoch: {} \tLoss: {:.4f} \tAcc: {:.4f}'.format(epoch, loss_avg, accuracy))
 
-            torch.save(self.state_dict(), 'model_{}_{:d}_{:d}'.format(epoch+1, int(loss_avg*100), int(accuracy*100)))
+            torch.save(self.state_dict(), 'model_d{:d}_e{}_l{:d}_a{:d}'.format(int(distribution[0]*100), epoch, int(loss_avg*100), int(accuracy*100)))
 
     def classify(self, data_set):
         classify_set = ClassifyLoader(data_set)
@@ -173,7 +121,7 @@ class LSTMNet(nn.Module):
 
     def test(self, test_set):
         test_set = TrainLoader(test_set)
-        test_generator = torch.utils.data.DataLoader(test_set, **self.params, collate_fn=test_set.pad_collate) # torch_geometric.data.DataLoader(test_set, **self.params)
+        test_generator = torch.utils.data.DataLoader(test_set, **self.params, collate_fn=test_set.pad_collate)
         # run a test loop
         test_loss = 0
         correct = 0
